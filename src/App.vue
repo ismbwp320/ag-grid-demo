a66d4663bb884523cfaa72959d5814e0c8dc3103
Ismail <ismbwp320@gmail.com>
2020-06-07 06:29:44 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,176 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div style="height: calc(100vh - 100px)">
+                <div class="example-header">
+                    Page Size:
+                    <select v-on:change="onPageSizeChanged()" id="page-size">
+                        <option value="10" selected="">10</option>
+                        <option value="100">100</option>
+                        <option value="500">500</option>
+                        <option value="1000">1000</option>
+                    </select>
+                </div>
+                <div style="margin-bottom: 5px;">
+                    <input type="text" v-on:input="onQuickFilterChanged()" id="quickFilter" placeholder="quick filter...">
+                </div>
+            <ag-grid-vue
+                style="width: 100%; height: 100%;"
+                class="ag-theme-alpine"
+                id="myGrid"
+                :gridOptions="gridOptions"
+                @grid-ready="onGridReady"
+                :columnDefs="columnDefs"
+                :defaultColDef="defaultColDef"
+                :modules="modules"
+                :pagination="true"
+                :paginationPageSize="paginationPageSize"
+                :sideBar="sideBar"
+                :rowData="rowData"></ag-grid-vue>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import { AgGridVue } from '@ag-grid-community/vue';
+    import { AllModules } from '@ag-grid-enterprise/all-modules';
+  
+    export default {
+        name: 'App',
+        data() {
+            return {
+      gridOptions: null,
+      gridApi: null,
+      columnApi: null,
+      defaultColDef: null,
+      columnDefs: null,
+      sideBar: null,
+      statusBar: null,
+      modules: AllModules,
+      enableRangeSelection: true,
+      rowData: null,
+              paginationPageSize: 10,
+              paginationNumberFormatter: null
+            }
+        },
+        components: {
+            AgGridVue
+        },
+  beforeMount() {
+    this.gridOptions = {};
+    this.defaultColDef = {
+      enableRowGroup: true,
+      enablePivot: true,
+      enableValue: true,
+      width: 140,
+      sortable: true,
+      resizable: true,
+      filter: true,
+    };
+    this.columnDefs = [
+    {
+        headerName: 'Shift Details',
+        children: [
+            { headerName: 'Site Name', field: 'athlete' },
+            { headerName: 'Type', field: 'age' },
+            { headerName: 'Officer', field: 'year' },
+            { headerName: 'Date', field: 'date' },
+            { headerName: 'Day', field: 'day' }
+        ]
+    },
+    {
+        headerName: 'Staff Details',
+        children: [
+            { headerName: 'Start', field: 'start' },
+            { headerName: 'End', field: 'end' },
+            { headerName: 'HR', field: 'hr' },
+            { headerName: 'Break', field: 'break' },
+            { headerName: 'Pay Rate', field: 'payrate'},
+            { headerName: 'Amount', field: 'amount', hide: true}
+        ]
+    },
+    {
+        headerName: 'Site Details',
+        children: [
+            { headerName: 'Site Start', field: 'start', hide: true },
+            { headerName: 'Site End', field: 'end', hide: true },
+            { headerName: 'Site HR', field: 'hr', hide: true },
+            { headerName: 'Change Rate', field: 'changeRate', hide: true },
+            { headerName: 'Change Amount', field: 'changeAmount', hide: true},
+            { headerName: 'Expense', field: 'expense', hide: true },
+             { headerName: 'Status', field: 'status', hide: true }
+             
+        ]
+    } 
+    ];
+    this.sideBar = {
+      toolPanels: [
+        'filters',
+        {
+          id: 'columns',
+          labelDefault: 'Columns',
+          labelKey: 'columns',
+          iconKey: 'columns',
+          toolPanel: 'agColumnsToolPanel',
+          toolPanelParams: { suppressSyncLayoutWithGrid: true },
+        },
+      ],
+    };
+    this.statusBar = {
+      statusPanels: [
+        {
+          statusPanel: 'agTotalRowCountComponent',
+          align: 'left',
+          key: 'totalRowComponent',
+        },
+        {
+          statusPanel: 'agFilteredRowCountComponent',
+          align: 'left',
+        },
+        {
+          statusPanel: 'agSelectedRowCountComponent',
+          align: 'center',
+        },
+        {
+          statusPanel: 'agAggregationComponent',
+          align: 'right',
+        },
+      ],
+    };
+    
+  },
+  mounted() {
+    this.gridApi = this.gridOptions.api;
+    this.gridColumnApi = this.gridOptions.columnApi;
+  },
+   methods: {
+         onQuickFilterChanged() {
+      this.gridApi.setQuickFilter(document.getElementById('quickFilter').value);
+    },
+    onPageSizeChanged() {
+      var value = document.getElementById('page-size').value;
+      this.gridApi.paginationSetPageSize(Number(value));
+    },
+    onGridReady(params) {
+      const httpRequest = new XMLHttpRequest();
+      const updateData = data => {
+        this.rowData = data;
+        params.api.paginationGoToPage(4);
+      };
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+      httpRequest.open(
+        'GET',
+        'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinnersSmall.json'
+      );
+      httpRequest.send();
+      httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+          updateData(JSON.parse(httpRequest.responseText));
+        }
+      };
+  }}
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,169 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <div style="height: calc(100vh - 100px)">
+                <div class="example-header">
+                    Page Size:
+                    <select v-on:change="onPageSizeChanged()" id="page-size">
+                        <option value="10" selected="">10</option>
+                        <option value="100">100</option>
+                        <option value="500">500</option>
+                        <option value="1000">1000</option>
+                    </select>
+                </div>
+            <ag-grid-vue
+                style="width: 100%; height: 100%;"
+                class="ag-theme-alpine"
+                id="myGrid"
+                :gridOptions="gridOptions"
+                @grid-ready="onGridReady"
+                :columnDefs="columnDefs"
+                :defaultColDef="defaultColDef"
+                :modules="modules"
+                :pagination="true"
+                :paginationPageSize="paginationPageSize"
+                :sideBar="sideBar"
+                :rowData="rowData"></ag-grid-vue>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    import { AgGridVue } from '@ag-grid-community/vue';
+    import { AllModules } from '@ag-grid-enterprise/all-modules';
+    import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+    import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+    import { MenuModule } from '@ag-grid-enterprise/menu';
+    import { SetFilterModule } from '@ag-grid-enterprise/set-filter';
+    import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+              gridOptions: null,
+              gridApi: null,
+              columnApi: null,
+              columnDefs: null,
+              defaultColDef: null,
+              defaultColGroupDef: null,
+              columnTypes: null,
+                    modules: [
+        ClientSideRowModelModule,
+        RowGroupingModule,
+        MenuModule,
+        SetFilterModule,
+        ColumnsToolPanelModule,
+        AllModules
+      ],
+              rowData: null,
+              sideBar: null,
+              paginationPageSize: 10,
+              paginationNumberFormatter: null
+            }
+        },
+        components: {
+            AgGridVue
+        },
+  beforeMount() {
+    this.gridOptions = {};
+    this.columnDefs = [
+      { field: 'athlete' },
+      {
+        field: 'age',
+        filter: 'agNumberColumnFilter',
+        maxWidth: 100,
+      },
+      { field: 'country' },
+      {
+        field: 'year',
+        maxWidth: 100,
+      },
+      {
+        field: 'date',
+        filter: 'agDateColumnFilter',
+        filterParams: {
+          comparator: (filterLocalDateAtMidnight, cellValue) => {
+            var dateAsString = cellValue;
+            if (dateAsString == null) return -1;
+            var dateParts = dateAsString.split('/');
+            var cellDate = new Date(
+              Number(dateParts[2]),
+              Number(dateParts[1]) - 1,
+              Number(dateParts[0])
+            );
+            if (filterLocalDateAtMidnight.getTime() == cellDate.getTime()) {
+              return 0;
+            }
+            if (cellDate < filterLocalDateAtMidnight) {
+              return -1;
+            }
+            if (cellDate > filterLocalDateAtMidnight) {
+              return 1;
+            }
+          },
+          browserDatePicker: true,
+        },
+      },
+      { field: 'sport' },
+      {
+        field: 'gold',
+        filter: 'agNumberColumnFilter',
+      },
+      {
+        field: 'silver',
+        filter: 'agNumberColumnFilter',
+      },
+      {
+        field: 'bronze',
+        filter: 'agNumberColumnFilter',
+      },
+      {
+        field: 'total',
+        filter: false,
+      },
+    ];
+    this.defaultColDef = {
+      flex: 1,
+      minWidth: 100,
+      editable: true,
+      filter: 'agTextColumnFilter',
+      floatingFilter: true,
+      resizable: true,
+      enablePivot: true,
+    };
+    this.sideBar = ['column', 'filter'];
+  },
+  mounted() {
+    this.gridApi = this.gridOptions.api;
+    this.gridColumnApi = this.gridOptions.columnApi;
+  },
+   methods: {
+    onPageSizeChanged() {
+      var value = document.getElementById('page-size').value;
+      this.gridApi.paginationSetPageSize(Number(value));
+    },
+    onGridReady(params) {
+      const httpRequest = new XMLHttpRequest();
+      const updateData = data => {
+        this.rowData = data;
+        params.api.paginationGoToPage(4);
+      };
+
+      httpRequest.open(
+        'GET',
+        'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinners.json'
+      );
+      httpRequest.send();
+      httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+          updateData(JSON.parse(httpRequest.responseText));
+        }
+      };
+    },
+  },
+    }
+</script>
+<style lang="scss">
+  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
 </style>
